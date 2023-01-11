0931643f0b282be707e3a95ef5edd4559c114729
Gleibson Oliveira <goliveira@olixcorp.com>
2023-01-11 14:42:03 -0300

    Initial commit

---
+++ src/main/java/br/com/grso/backendattornatus/BackendAttornatusApplication.java
@@ -0,0 +1,13 @@
+package br.com.grso.backendattornatus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendAttornatusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendAttornatusApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/grso/backendattornatus/BackendAttornatusApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.grso.backendattornatus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendAttornatusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
